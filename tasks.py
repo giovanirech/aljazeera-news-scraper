b4b42b9db3a7227d78cae8f28b8f484fd53d65e7
Giovani Rech <gio.pi.rech@gmail.com>
2024-09-30 18:23:35 -0300

    Refactor NewsScraper to log collected workitems and fix minor formatting issue

--- tasks.py
+++ tasks.py
@@ -9,17 +9,17 @@ TARGET_WEBSITE = "https://www.aljazeera.com/"
 
 @task
 def make_aljazeera_news_report():
-    
-    scraper = NewsScraper()
 
+    scraper = NewsScraper()
     try:
         scraper.set_output_dir(OUTPUT_DIR / "aljazeera_news")
         scraper.prepare_environment(browser="Chrome")
         scraper.access_home_page(TARGET_WEBSITE)
-
+        
         for item in workitems.inputs:
             search_phrase = item.payload.get("search_phrase", "Science")
             number_of_months = item.payload.get("number_of_months", 1)
+            scraper.logger.info(f"Collected workitems: {search_phrase}, {number_of_months}")
             scraper.collect_articles(search_phrase, number_of_months=number_of_months)
 
         scraper.create_report()
